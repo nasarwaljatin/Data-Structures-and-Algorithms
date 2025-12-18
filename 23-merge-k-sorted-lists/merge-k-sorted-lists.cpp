4d48d31100421d2587afb43a81809d4e01d4a55e
nasarwaljatin <nasarwaljatin@gmail.com>
2025-12-18 17:09:18 +0530

    Time: 102 ms (12.22%) | Memory: 18.5 MB (47.51%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -13,13 +13,11 @@
     while(list1 && list2){
         if(list1->val >=list2->val){
             temp->next=list2;
-            temp=list2;
             list2=list2->next;
         }else{
             temp->next=list1;
-            temp=list1;
             list1=list1->next;
-        }
+        }temp=temp->next;
     }if(list1) temp->next=list1;
         else temp->next=list2;
     return dummy->next;
