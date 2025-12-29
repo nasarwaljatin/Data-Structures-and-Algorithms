ea65f6148930db39564cc5524eb42f29d72a74ba
nasarwaljatin <nasarwaljatin@gmail.com>
2025-12-29 19:53:41 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (39.11%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    void fn(int i,set<vector<int>>& ans,vector<int>& ds,vector<int>& c){
-    if(i==c.size()){
-        ans.insert(ds);
-        return;
-    }ds.push_back(c[i]);
-    fn(i+1,ans,ds,c);
-    ds.pop_back();
-    fn(i+1,ans,ds,c);
-}
+    void fn(int k, set<vector<int>>& ans, vector<int>& ds, vector<int>& c) {
+            ans.insert(ds);
+        for (int i = k; i < c.size(); i++) {
+            ds.push_back(c[i]);
+            fn(i + 1, ans, ds, c);
+            ds.pop_back();
+        }
+    }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(), nums.end());
         vector<vector<int>> ansi;
         set<vector<int>> ans;
         vector<int> ds;
-        fn(0,ans,ds,nums);
-        for(auto it:ans){
+        fn(0, ans, ds, nums);
+        for (auto it : ans) {
             ansi.push_back(it);
         }
         return ansi;
