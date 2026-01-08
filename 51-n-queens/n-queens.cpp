cb08c5df4d96f26531fd5a62fd1a7c95b1e1937d
nasarwaljatin <nasarwaljatin@gmail.com>
2026-01-08 13:01:23 +0530

    Time: 3 ms (62.84%) | Memory: 10.3 MB (56.67%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
-    void solve(int col,vector<string>& board,vector<vector<string>>& ans,int n){
+    bool safe(int row,int col,vector<string> &board,int n){
+        int drow=row,dcol=col;
+        while(col>=0){
+            if(board[row][col]=='Q') return false;
+            col--;
+        }row=drow,col=dcol;
+        while(row<n && col>=0){
+            if(board[row][col]=='Q') return false;
+            row++;col--;
+        }row=drow,col=dcol;
+        while(row>=0 && col>=0){
+            if(board[row][col]=='Q') return false;
+            row--;col--;
+        }return true;
+    }
+    void fn(vector<string> &board,int n,int col,vector<vector<string>> &ans){
         if(col==n){ans.push_back(board);return;}
         for(int row=0;row<n;row++){
-            if(safe(row,col,n,board)){
+            if(safe(row,col,board,n)){
                 board[row][col]='Q';
-                solve(col+1,board,ans,n);
+                fn(board,n,col+1,ans);
                 board[row][col]='.';
             }
         }
     }
-    bool safe(int row,int col,int n,vector<string>& board){
-        int duprow=row,dupcol=col;
-        while(row>=0 && col>=0){
-            if(board[row][col]=='Q') return false;
-            row--;col--;
-        }col=dupcol;row=duprow;
-        while(col>=0){
-            if(board[row][col]=='Q') return false;
-            col--;
-        }row=duprow;col=dupcol;
-        while(row<n && col>=0){
-            if(board[row][col]=='Q') return false;
-            row++;col--;
-        }return true;
-    }
     vector<vector<string>> solveNQueens(int n) {
-        vector<string> board(n);
-        string s(n,'.');
+        vector<string> board(n,string(n,'.'));
         vector<vector<string>> ans;
-        for(int i=0;i<n;i++){board[i]=s;}
-        solve(0,board,ans,n);
+        fn(board,n,0,ans);
         return ans;
     }
 };
