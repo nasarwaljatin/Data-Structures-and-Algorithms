4cf6626a6a90afb9f2ad767119a54e9b7bc75545
nasarwaljatin <nasarwaljatin@gmail.com>
2026-01-24 09:48:24 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (64.27%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -3,7 +3,8 @@ public:
     string largestOddNumber(string num) {
         int n=num.size(),i=0,j=n-1;
         while(j>=0 && i<=j){
-            if(num[j]%2==0) j--;
+            if((num[j] - '0') % 2
+==0) j--;
             else break;
         }if(j<0) return "";
         return num.substr(i,j-i+1);
