ac43175c316a86c358a1811798c82ae3fe888a6c
nasarwaljatin <nasarwaljatin@gmail.com>
2026-01-25 09:51:21 +0530

    Time: 55 ms (15.32%) | Memory: 89 MB (8.70%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -5,13 +5,13 @@ public:
         map<int,int> mpp;
         while(j<n && i<n){
             mpp[nums[j]]++;
-            while(mpp.size()>2){
+            if(mpp.size()>2){
                 mpp[nums[i]]--;
                 if (mpp[nums[i]] == 0)
                     mpp.erase(nums[i]);
                 i++;
             }
-            maxi=max(maxi,j-i+1);
+            if(mpp.size()<=2) maxi=max(maxi,j-i+1);
             j++;
         }return maxi;
     }
