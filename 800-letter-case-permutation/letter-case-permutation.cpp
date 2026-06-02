05a35d4c20201a8bf46a53a900ae05867473d40c
Jatin <nasarwaljatin@gmail.com>
2026-06-02 10:31:37 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (56.50%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.cpp
+++ 800-letter-case-permutation/letter-case-permutation.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
-    void fn(string s,vector<string>& ans,string d,int i){
+    void fn(const string& s,vector<string>& ans,string& d,int i){
         if(i>=s.size()){ans.push_back(d);return;}
         if(isdigit(s[i])){
             d.push_back(s[i]);
             fn(s,ans,d,i+1);
+            d.pop_back();
         }else{
             if(isupper(s[i])){
                 d.push_back(tolower(s[i]));
@@ -12,12 +13,14 @@ public:
                 d.pop_back();
                 d.push_back(toupper(s[i]));
                 fn(s,ans,d,i+1);
+                d.pop_back();
             }else{
                 d.push_back(toupper(s[i]));
                 fn(s,ans,d,i+1);
                 d.pop_back();
                 d.push_back(tolower(s[i]));
                 fn(s,ans,d,i+1);
+                d.pop_back();
             }
         }
     }
