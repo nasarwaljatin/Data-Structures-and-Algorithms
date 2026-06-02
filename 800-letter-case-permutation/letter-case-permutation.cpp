685bc0d113bc4598b9a1b62d781af97b506f9e43
Jatin <nasarwaljatin@gmail.com>
2026-06-02 10:02:40 +0530

    Time: 4 ms (34.01%) | Memory: 15.8 MB (13.90%) - LeetSync

---
+++ 800-letter-case-permutation/letter-case-permutation.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void fn(string s,vector<string>& ans,string d,int i){
+        if(i>=s.size()){ans.push_back(d);return;}
+        if(isdigit(s[i])){
+            d.push_back(s[i]);
+            fn(s,ans,d,i+1);
+        }else{
+            if(isupper(s[i])){
+                d.push_back(tolower(s[i]));
+                fn(s,ans,d,i+1);
+                d.pop_back();
+                d.push_back(toupper(s[i]));
+                fn(s,ans,d,i+1);
+            }else{
+                d.push_back(toupper(s[i]));
+                fn(s,ans,d,i+1);
+                d.pop_back();
+                d.push_back(tolower(s[i]));
+                fn(s,ans,d,i+1);
+            }
+        }
+    }
+    vector<string> letterCasePermutation(string s) {
+        vector<string> ans;
+        string d;
+        fn(s,ans,d,0);
+        return ans;
+    }
+};
