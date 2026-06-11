875b4747088fc897f6dda0a1b504bb5e850266bb
Jatin <nasarwaljatin@gmail.com>
2026-06-11 22:01:34 +0530

    Time: 34 ms (42.25%) | Memory: 61.8 MB (5.79%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int change(int d, vector<int>& nums) {
-        int n = nums.size();
-        vector<unsigned long long> dp(d + 1, 0);
-        dp[0] = 1;
-        for (int i = 1; i <= n; i++) {
-            for (int j = nums[i - 1]; j <= d; j++) {
-                dp[j] += dp[j - nums[i - 1]];
+        int n=nums.size(),c=0;
+        vector<vector<unsigned long long>> dp(n+1,vector<unsigned long long>(d+1,0));
+        for(int i=0;i<=n;i++) dp[i][0]=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=d;j++){
+                if(nums[i-1]<=j) dp[i][j]=dp[i-1][j]+dp[i][j-nums[i-1]];
+                else dp[i][j]=dp[i-1][j];
             }
-        }return dp[d];
+        }
+        return dp[n][d];
     }
 };
