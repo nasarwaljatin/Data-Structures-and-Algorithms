478febfd78bc193376a641fd4746e1d8568078cd
Jatin <nasarwaljatin@gmail.com>
2026-06-16 18:13:45 +0530

    Time: 66 ms (28.98%) | Memory: 32.5 MB (25.05%) - LeetSync

--- 923-super-egg-drop/super-egg-drop.cpp
+++ 923-super-egg-drop/super-egg-drop.cpp
@@ -4,16 +4,16 @@ public:
     int fn(int n,int f){
         if(n==1||f<=1) return f;
         if(dp[n][f]!=-1) return dp[n][f];
-        int mini=INT_MAX;
+        dp[n][f]=INT_MAX;
         int low=1,high=f;
         while(low<=high){
             int a,b,i=(low+high)>>1;
             a=dp[n-1][i-1]=fn(n-1,i-1);;
             b=dp[n][f-i]=fn(n,f-i);
-            mini=min(mini,1+max(a,b));
+            dp[n][f]=min(dp[n][f],1+max(a,b));
             if(a<b) low=i+1;
             else high=i-1;
-        }return dp[n][f]=mini;
+        }return dp[n][f];
     }
     int superEggDrop(int n, int f) {
         dp.resize(n+1,vector<int>(f+1,-1));
