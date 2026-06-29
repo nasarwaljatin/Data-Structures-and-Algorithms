7f123c70f71f78033d0e5bea965c2f4250582e6c
Jatin <nasarwaljatin@gmail.com>
2026-06-29 10:01:20 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (72.86%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     vector<int> visited;
-    void dfs(vector<list<int>>& nums,int u){
+    void dfs(vector<vector<int>>& nums,int u){
         visited[u]=true;
-        for(int v:nums[u]) if(!visited[v]) dfs(nums,v);
-        return;
+        for(int v=0;v<nums.size();v++){
+            if(nums[u][v] && !visited[v]) dfs(nums,v);
+        }
     }
     int findCircleNum(vector<vector<int>>& num) {
         visited.resize(num.size(),0);
         int c=0;
-        vector<list<int>> nums(num.size());
         for(int i=0;i<num.size();i++){
-            for(int j=i+1;j<num.size();j++){
-                if(num[i][j]) {nums[i].push_back(j);nums[j].push_back(i);}
-            }
-        }
-        for(int i=0;i<num.size();i++){
-            if(!visited[i]){ dfs(nums,i); c++; }
+            if(!visited[i]){ dfs(num,i); c++; }
         }return c;
     }
 };
