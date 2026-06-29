b5848f4db5687ca85f91026402779b44e453db9d
Jatin <nasarwaljatin@gmail.com>
2026-06-29 10:52:09 +0530

    Time: 4 ms (7.10%) | Memory: 20.1 MB (13.19%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
@@ -14,7 +14,7 @@ public:
     int widthOfBinaryTree(TreeNode* root) {
         long long maxi=0;
         queue<pair<TreeNode*,long long>> q;
-        q.push({root,0});
+        q.push({root,1});
         while(!q.empty()){
             long long n=q.size();
             vector<long long> ans;
@@ -22,8 +22,8 @@ public:
             for(int i=0;i<n;i++){
                 TreeNode* temp=q.front().first;
                 long long k=q.front().second-mini;q.pop();
-                if(temp->left) q.push({temp->left,2 * k + 1});
-                if(temp->right) q.push({temp->right,2 * k + 2});
+                if(temp->left) q.push({temp->left,2 * k });
+                if(temp->right) q.push({temp->right,2 * k + 1});
                 ans.push_back(k);
             }maxi = max(ans.back() - ans.front() + 1, maxi);
         }return maxi;
