1a49b4b99c59d0a6148404e7fc38b05685a1447a
Jatin Nasarwal <nasarwaljatin@gmail.com>
2026-09-11 10:05:19 +0530

    Time: 0 ms (100.00%) | Memory: 26.8 MB (38.61%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     int trap(vector<int>& nums) {
-        int n=nums.size(),maxa=-1,maxb=-1,sum=0;
-        vector<int> a(n,-1),b(n,-1);
-        for(int i=1;i<n;i++){
-            a[i]=max(a[i-1],nums[i-1]);
-        }for(int i=n-2;i>=0;i--){
-            b[i]=max(b[i+1],nums[i+1]);
+        int n=nums.size(),maxi=0,sum=0;
+        vector<int> numi(n);
+        for(int i=0;i<n;i++){
+            maxi=max(maxi,nums[i]);
+            numi[i]=maxi;
+        }maxi=0;for(int i=n-1;i>=0;i--){
+            maxi=max(maxi,nums[i]);
+            numi[i]=min(numi[i],maxi);
         }for(int i=0;i<n;i++){
-            if(nums[i]<min(a[i],b[i])) sum+=min(a[i],b[i])-nums[i];
+            if(numi[i]<nums[i]) continue;
+            else{
+                sum+=(numi[i]-nums[i]);
+            }
         }return sum;
+
     }
 };
